bf72d1aee9e7bb51e38152f0fd246c78f2d26603
Matthew Cook <cookm353@gmail.com>
2022-11-03 13:37:39 -0400

    adding basic login/logout routes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, abort, request, g, flash
+from flask import Flask, render_template, abort, request, g, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, User
+from models import connect_db, User, Drink
 
 app = Flask(__name__)
+CURR_USER_KEY = 'curr_user'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///drinkr'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,4 +16,42 @@ connect_db(app)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+# Sign up, login, logout
+
+@app.before_request
+def add_user_to_g():
+    """Add curr user to global if user is logged in"""
+    
+    if CURR_USER_KEY in session:
+        g.user = User.get(session[CURR_USER_KEY])
+    else:
+        g.user = None
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     ...
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    ...
+    
+@app.route('/logout')
+def logout():
+    ...
+
+
+
+@app.route('/user/<int:userId>')
+def user_page(userId):
+    user = User.get(userId)
+    ...
+    
+@app.route('/drinks')
+def show_drinks():
+    ...
+    
+@app.route('/drinks/<int:drinkId>')
+def show_drink(drinkId):
+    drink = Drink.get(drinkId)
