419db0bc1c79b177b0c024d583972000603452dc
Matthew Cook <cookm353@gmail.com>
2022-11-05 15:14:14 -0400

    glasses, users, ingredients, and drinks added

--- seed.py
+++ seed.py
@@ -2,9 +2,6 @@ from app import app
 from models import db, User, Glass, CustomDrink, Ingredient, Drink, DrinkIngredient
 import requests
 
-db.drop_all()
-db.create_all()
-
 def addUsers():
     User.query.delete()
 
@@ -18,58 +15,56 @@ def addUsers():
     User.register(aliceData)
     User.register(bobData)
 
-def getGlasses():
+def addGlasses():
     Glass.query.delete()
 
-    url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list"
-
-    glassesResp = requests.get(url).json()['drinks']
-    glasses = [glass['strGlass'] for glass in glassesResp]
-
+    with open('seed_files/glasses.txt', 'r') as file:
+        glasses = file.readlines()
+        
+    glasses = [glass.replace('\n', '').strip() for glass in glasses]
+    
     for glass in glasses:
         newGlass = Glass(name=glass)
         db.session.add(newGlass)
-        
-    db.session.commit()
-    
 
-def getIngredients():
+def addIngredients():
     Ingredient.query.delete()
 
-    url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+    with open('seed_files/ingredients.txt', 'r') as file:
+        ingredients = file.readlines()
+        
+    ingredients = [ingredient.replace('\n', '').strip() for ingredient in ingredients]
 
-    ingredientsResp = requests.get(url).json()['drinks']
-    ingredients = [ingredient['strIngredient'] for ingredient in ingredientsResp]
-
+    
     for ingredient in ingredients:
-        print(ingredient)
         newIngredient = Ingredient(name=ingredient)
         db.session.add(newIngredient)
-        
-    db.session.commit()
 
-
-def getDrinks():
+def addDrinks():
     Drink.query.delete()
     
     with open('seed_files/drinks.txt', 'r') as file:
         drinks = file.readlines()
         
-    drinks = [drink.replace(' \n', '') for drink in drinks]
+    drinks = [drink.replace('\n', '').strip() for drink in drinks]
         
     for drink in drinks:
         newDrink = Drink(name=drink)
-        
         db.session.add(newDrink)
-        
-    db.session.commit()
     
 
 def main():
+    # Reset tables
+    db.drop_all()
+    db.create_all()
+    
     addUsers()
-    getGlasses()
-    getIngredients()
-    getDrinks()
+    addGlasses()
+    addIngredients()
+    addDrinks()
+    
+    # Commit changes
+    db.session.commit()
     
 
 if __name__ == "__main__":
