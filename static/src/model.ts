5c33a974e6696f1090ac148a6ed707553c5d5f55
Matthew Cook <cookm353@gmail.com>
2022-12-02 16:27:14 -0500

    creating cabinet model

---
+++ static/src/model.ts
@@ -0,0 +1,40 @@
+const $ingredientToAdd = $('#ingredientList')
+const $ingredientToRemove = $('#remove-bottle')
+
+interface cabinetJSON {
+    ingredientID: string
+}
+
+// Class to handle adding and removing bottles from cabinet
+class Cabinet {
+    URL: string = this.getURL();
+
+    // Create base URL for requests
+    getURL(): string {
+        const pageURL: string = window.location.href
+        const index: number = pageURL.search('/cab') - 1
+        const userId: string = pageURL[index]
+        
+        return(`/user/${userId}/cabinet`);
+    }
+
+    async addBottle() {
+    // Handle adding a bottle to a user's cabinet
+        const data: cabinetJSON = {
+            ingredientID: $ingredientToAdd.val()
+        }
+
+        // console.log(this.URL)
+        // console.log(data)
+        await axios.post(this.URL, data)
+    }
+    
+    async removeBottle(ingredientID: string) {
+        const data: cabinetJSON = {
+            ingredientID: ingredientID
+        }
+
+        const deleteURL = `${this.URL}/delete`
+        await axios.post(deleteURL, data)
+    }
+}
